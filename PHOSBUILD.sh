43f0f3d4bc32887e337e3f47692f101895fcd691
The Phantom Technology -Advanced Pentest <155259033+The-Phantom-Technology@users.noreply.github.com>
2024-12-01 16:03:57 +0800

    更新

---
+++ PHOSBUILD.sh
@@ -0,0 +1,21 @@
+rm -rf /opt/PHANTX/sqlmap
+
+cd /opt/PHANTX
+
+https://github.com/nmap/nmap
+
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+make
+make install
+
+chmod -R 755 /opt/PHANTX
+rm -rf /opt/PHOS/tool/sqlmap
